9d1a1053d748a7f592298b5aedbc301effffdd0f
AgeenkoKonstantin <70561130+AgeenkoKonstantin@users.noreply.github.com>
2023-09-05 21:01:45 +0300

    bookCategoryController, Service unit test, nelmio doc

---
+++ tests/Service/BookCategoryServiceTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Tests\Service;
+
+use App\Entity\BookCategory;
+use App\Model\BookCategoryListItem;
+use App\Model\BookCategoryListResponse;
+use App\Repository\BookCategoryRepository;
+use App\Service\BookCategoryService;
+use Doctrine\Common\Collections\Criteria;
+use PHPUnit\Framework\TestCase;
+
+class BookCategoryServiceTest extends TestCase
+{
+
+    public function testGetCategories(): void
+    {
+        $repository = $this->createMock(BookCategoryRepository::class);
+        $repository->expects($this->once())
+            ->method('findBy')
+            ->with([],['title'=> Criteria::ASC])
+            ->willReturn([(new BookCategory())->setTitle('Test')->setSlug("test")->setId(1)]);
+
+        $expected = new BookCategoryListResponse([new BookCategoryListItem(1, 'Test', 'test')]);
+
+        $service = new BookCategoryService($repository);
+        $actual = $service->getCategories();
+
+        $this->assertEquals($expected, $actual);
+    }
+}
