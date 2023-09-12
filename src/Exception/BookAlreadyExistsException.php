40adc6907a22d75876701f24183ca8e4b7f35dcb
AgeenkoKonstantin <70561130+AgeenkoKonstantin@users.noreply.github.com>
2023-09-12 18:35:25 +0300

    API for authors

---
+++ src/Exception/BookAlreadyExistsException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Exception;
+
+class BookAlreadyExistsException extends \RuntimeException
+{
+    public function __construct()
+    {
+        parent::__construct('Book already exists');
+    }
+
+}
